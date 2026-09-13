4b9ca1d0f788c258bcaf605ddc42bb6e5d900900
Nadia-2005-code <oak200511@gmail.com>
2026-09-13 15:18:24 +0100

    Added data directory with site info type script file following information gathered from client

---
+++ src/data/siteInf.ts
@@ -0,0 +1,10 @@
+export const siteInfo = {
+  name: "F&B Painting & Decorating Services",
+  phone: "07418 064988",
+  email: "fandbpaintingdecorating@gmail.com",
+  socials: {
+    instagram: "https://instagram.com/fandbpaintingdecorating",
+    facebook: "https://facebook.com/fandbpaintingdecorating",
+  },
+  coverage: ["Wirral", "Liverpool", "Chester", "Ellesmere Port"],
+};
