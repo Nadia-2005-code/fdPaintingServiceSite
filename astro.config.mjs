6b3fdb705ea3cdd54cece1492833f7e93a17ea4f
Nadia-2005-code <oak200511@gmail.com>
2026-09-15 22:24:42 +0100

    Improved page speed by following recommended instructions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://fbpaintinganddecorating.co.uk',
+  trailingSlash: 'never',
   integrations: [sitemap()],
 });
