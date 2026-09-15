28001f9758365874179975fc90b88732ad18b053
Nadia-2005-code <oak200511@gmail.com>
2026-09-15 23:26:03 +0100

    Improved render blocking in config and tried to get main image to line up with the width that is expected on mobile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://fbpaintinganddecorating.co.uk',
   trailingSlash: 'never',
   integrations: [sitemap()],
+  build: {
+    inlineStylesheets: 'always'
+  }
 });
