31539469522a64673609c43d55e2027d28f9c32f
Nadia-2005-code <oak200511@gmail.com>
2026-09-16 15:33:46 +0100

    Further optimised codebase, mainly by consolidating stripping of phone number to correct form into siteInfo.ts

--- src/data/siteInfo.ts
+++ src/data/siteInfo.ts
@@ -1,6 +1,10 @@
+const phone = "07418 064988";
+
 export const siteInfo = {
   name: "F&B Painting & Decorating Services",
-  phone: "07418 064988",
+  phone: phone,
+  phoneRaw: phone.replace(/\s+/g, ''),
+  whatsappRaw: phone.replace(/^0/, '44').replace(/\s+/g, ''),
   email: "fandbpaintingdecorating@gmail.com",
   socials: {
     instagram: "https://instagram.com/fandbpaintingdecorating",
